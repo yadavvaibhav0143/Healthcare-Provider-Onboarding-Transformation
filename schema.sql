966cebafb712cfa777e6e7283c361f20882bb7de
Vaibhav Yadav <yadavvaibhav0143@gmail.com>
2026-06-22 20:39:13 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -65,4 +65,8 @@ CREATE TABLE OnboardingTransactions (
     CONSTRAINT fk_registration_number
     FOREIGN KEY (registration_number)
     REFERENCES RegistriesMaster(registration_number)
+
+    CONSTRAINT chk_onboarding_dates 
+        CHECK (COALESCE(approval_timestamp, verification_timestamp, submission_timestamp) >= submission_timestamp)   
+
 );
